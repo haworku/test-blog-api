01d5c1a2ec0d1a18c0b254fbf633c2537b85a8a0
Hana Worku <haworku@gmail.com>
2020-07-06 10:27:50 -0500

    UserController uses async await for all methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,56 +1,66 @@
 const User = require("../models/Users");
 
-exports.listAllUsers = (req, res) => {
-  User.find({}, (err, user) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(200).json(user);
-  });
+exports.listAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 exports.createNewUser = async (req, res) => {
-  const newUser = req.body;
-  const user = new User(newUser);
+  const user = new User(req.body);
   try {
     await user.save();
     res.status(201).json(user);
   } catch (err) {
-    if (err) {
+    if (err.code === 11000 && err.keyPattern.hasOwnProperty("username")) {
+      res.status(500).send({
+        message: `Two users cannot share the same username (${err.keyValue.username}).`,
+      });
+    } else {
       res.status(500).send(err);
-      // TODO: Add specific error message when creating a user with duplicate username
     }
   }
 };
 
-exports.updateUser = (req, res) => {
-  User.findOneAndUpdate(
-    { _id: req.params.userid },
-    req.body,
-    { new: true },
-    (err, user) => {
-      if (err) {
-        res.status(500).send(err);
+exports.updateUser = async (req, res) => {
+  try {
+    const newUser = await User.findOneAndUpdate(
+      { _id: req.params.userid },
+      req.body,
+      {
+        new: true,
       }
-      res.status(200).json(user);
-    }
-  );
-};
-
-exports.deleteUser = (req, res) => {
-  User.deleteOne({ _id: req.params.userid }, (err, user) => {
-    if (err) {
-      res.status(404).send(err);
-    }
-    res.status(204).json({ message: "User successfully deleted" });
-  });
-};
-
-exports.getUser = (req, res) => {
-  User.findById(req.params.userid, (err, user) => {
-    if (err) {
+    );
+    res.status(200).json(newUser);
+  } catch (err) {
+    if (err.code === 11000 && err.keyPattern.hasOwnProperty("username")) {
+      res.status(500).send({
+        message: `Two users cannot share the same username (${err.keyValue.username}).`,
+      });
+    } else {
       res.status(500).send(err);
     }
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.deleteOne({ _id: req.params.userid });
+    if (!user) res.status(404).send("No user found");
+    res.status(204).json({ message: "User successfully deleted" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userid);
     res.status(200).json(user);
-  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
